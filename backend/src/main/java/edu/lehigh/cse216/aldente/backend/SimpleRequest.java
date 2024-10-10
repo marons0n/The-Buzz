ce266d181e2564b840f889a5f4623577652af752
Matt Aronson <maa326@lehigh.edu>
2024-10-10 16:39:22 -0400

    deleted old files

--- backend/src/main/java/edu/lehigh/cse216/aldente/backend/SimpleRequest.java
+++
@@ -1,9 +0,0 @@
-package edu.lehigh.cse216.aldente.backend;
-
-/**
- * SimpleRequest provides a format for clients to present title and message 
- * strings to the server.
- * @param mTitle The title being provided by the client.
- * @param mMessage The message being provided by the client.
- */
-public record SimpleRequest( String mTitle, String mMessage ) {}
