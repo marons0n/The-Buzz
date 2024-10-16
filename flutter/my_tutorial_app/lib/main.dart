f776ca2a1186b147b304ebdf2bf071ac0bfc8565
Stefania Dzhaman <dzhaman3@illinois.edu>
2024-10-15 22:21:59 -0400

    likes and display working, adding idea kind of works

--- flutter/my_tutorial_app/lib/main.dart
+++ flutter/my_tutorial_app/lib/main.dart
@@ -1,10 +1,31 @@
 import 'package:flutter/material.dart';
-import 'package:english_words/english_words.dart';
 import 'dart:developer' as developer;
 import 'dart:convert';
+import './models/IdeaItem.dart';
+import './net/webRequests.dart';
 import 'package:http/http.dart' as http;
-import './models/NumberWordPair.dart';
-import './models/webRequests.dart';
+
+// example modified from https://pub.dev/packages/http
+import 'package:http/http.dart' as http;
+
+Future<void> doRequests() async {
+  var url = Uri.https('example.com', 'whatsit/create');             // using https; http also possible
+  var response = await http.post(                                   // POST returning Future<Response>
+                  url, 
+                  body: {'name': 'doodle', 'color': 'blue'},
+                  headers: {});
+  print('Response status: ${response.statusCode}');
+  print('Response body: ${response.body}');
+
+  var url2 = Uri.http('www.cse.lehigh.edu', '~spear/courses.json');  // using http
+  var response2 = await http.get(
+                  url2, 
+                  headers: {});                                      // GET returning Future<Response>
+  print('Response2 status: ${response2.statusCode}');
+  print('Response2 body: ${response2.body}');
+
+  print(await http.read(Uri.https('example.com', 'foobar.txt')));    // GET returning Future<String>
+}
 
 void main() {
   runApp(const MyApp());
@@ -12,6 +33,7 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
@@ -21,7 +43,7 @@ class MyApp extends StatelessWidget {
         // This is the theme of your application.
         //
         // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
+        // the application has a blue toolbar. Then, without quitting the app,
         // try changing the seedColor in the colorScheme below to Colors.green
         // and then invoke "hot reload" (save your changes or press the "hot
         // reload" button in a Flutter-supported IDE, or press "r" if you used
@@ -36,27 +58,32 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'The Buzz'),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
+
   // This widget is the home page of your application. It is stateful, meaning
   // that it has a State object (defined below) that contains fields that affect
   // how it looks.
+
   // This class is the configuration for the state. It holds the values (in this
   // case the title) provided by the parent (in this case the App widget) and
   // used by the build method of the State. Fields in a Widget subclass are
   // always marked "final".
+
   final String title;
+
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
+
   void _incrementCounter() {
     setState(() {
       // This call to setState tells the Flutter framework that something has
@@ -68,7 +95,7 @@ class _MyHomePageState extends State<MyHomePage> {
     });
   }
 
-  @override
+    @override
   Widget build(BuildContext context) {
     // This method is rerun every time setState is called, for instance as done
     // by the _incrementCounter method above.
@@ -81,8 +108,7 @@ class _MyHomePageState extends State<MyHomePage> {
         // TRY THIS: Try changing the color here to a specific color (to
         // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
         // change color while the other colors stay the same.
-        // backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        backgroundColor: Colors.blue,
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         // Here we take the value from the MyHomePage object that was created by
         // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
@@ -91,199 +117,144 @@ class _MyHomePageState extends State<MyHomePage> {
         // Center is a layout widget. It takes a single child and positions it
         // in the middle of the parent.
         child: HttpReqWords(),
-      ),
+      ), // This trailing comma makes auto-formatting nicer for build methods.      
     );
   }
+
 }
 
 class HttpReqWords extends StatefulWidget {
   const HttpReqWords({super.key});
+
   @override
   State<HttpReqWords> createState() => _HttpReqWordsState();
 }
 
+// note: you will have to add the following at the top of the file for developer.log to work:
+// import 'dart:developer' as developer;
+
 class _HttpReqWordsState extends State<HttpReqWords> {
-  // final _words = <String>[];
   var _words = <String>['a', 'b', 'c', 'd'];
   final _biggerFont = const TextStyle(fontSize: 18);
-  late Future<List<NumberWordPair>> _future_words;
-  late Future<List<NumberWordPair>> _future_list_numword_pairs;
+  late Future<List<IdeaItem>> _future_list_ideas;
+  String _newIdea = ''; // Variable to hold the input text
+
   @override
   void initState() {
     super.initState();
-    _future_words = doSomeLongRunningCalculation();
-    _future_list_numword_pairs = getWebData();
+    _future_list_ideas = fetchIdeas();
   }
 
   void _retry() {
     setState(() {
-      _future_list_numword_pairs = getWebData();
+      _future_list_ideas = fetchIdeas();
     });
+  }
+
+  Future<void> _submitIdea() async {
+    final newIdea = _newIdea.trim();
+    if (newIdea.isNotEmpty) {
+      try {
+        await submitNewIdea(newIdea);
+        setState(() {
+          _newIdea = ''; // Clear the input after submission
+        });
+        _retry(); // Refresh the list of ideas
+      } catch (e) {
+        print("Error submitting idea: $e");
+        // Optionally, show an error message to the user
+      }
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    return build_v3(context);
-  }
-
-  Widget build_v3(BuildContext context) {
-    var fb = FutureBuilder<List<NumberWordPair>>(
-      future: _future_list_numword_pairs,
-      builder:
-          (BuildContext context, AsyncSnapshot<List<NumberWordPair>> snapshot) {
-        Widget child;
-        if (snapshot.hasData) {
-          // developer.log('`using` ${snapshot.data}', name: 'my.app.category');
-          // create  listview to show one row per array element of json response
-          child = ListView.builder(
-              //shrinkWrap: true, //expensive! consider refactoring. https://api.flutter.dev/flutter/widgets/ScrollView/shrinkWrap.html
-              padding: const EdgeInsets.all(16.0),
-              itemCount: snapshot.data!.length,
-              itemBuilder: /*1*/ (context, i) {
-                return Column(
-                  children: <Widget>[
-                    ListTile(
-                      title: Text(
-                        "Idea ${i}: num=${snapshot.data![i].mLikes} str=${snapshot.data![i].mId}",
-                        // snapshot.data![i].str,
-                        style: _biggerFont,
-                      ),
-                    ),
-                    Divider(height: 1.0),
-                    const Expanded(
-                      child: TextField(
-                        decoration: InputDecoration(
-                          labelText: 'Enter a new idea',
-                          border: OutlineInputBorder(),
+    return Column(
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Row(
+            children: [
+              Expanded(
+                child: TextField(
+                  onChanged: (value) {
+                    setState(() {
+                      _newIdea = value; // Update the input state
+                    });
+                  },
+                  decoration: InputDecoration(
+                    labelText: 'Enter a new idea',
+                    border: OutlineInputBorder(),
+                  ),
+                  // Optionally, you can also add a onSubmitted callback
+                  onSubmitted: (_) => _submitIdea(),
+                ),
+              ),
+              IconButton(
+                icon: Icon(Icons.send),
+                onPressed: _submitIdea,
+              ),
+            ],
+          ),
+        ),
+        Expanded(
+          child: FutureBuilder<List<IdeaItem>>(
+            future: _future_list_ideas,
+            builder: (BuildContext context, AsyncSnapshot<List<IdeaItem>> snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return const Center(child: CircularProgressIndicator());
+              } else if (snapshot.hasError) {
+                return Text('Error: ${snapshot.error}');
+              } else if (snapshot.hasData) {
+                return ListView.builder(
+                  padding: const EdgeInsets.all(16.0),
+                  itemCount: snapshot.data!.length,
+                  itemBuilder: (context, i) {
+                    final idea = snapshot.data![i];
+                    return Column(
+                      children: <Widget>[
+                        ListTile(
+                          title: Text(
+                            "ID: ${idea.mId}, Message: ${idea.mMessage}",
+                            style: _biggerFont,
+                          ),
+                          trailing: Row(
+                            mainAxisSize: MainAxisSize.min,
+                            children: [
+                              Text("${idea.mLikes}"),
+                              IconButton(
+                                icon: Icon(Icons.thumb_up),
+                                onPressed: () async {
+                                  try {
+                                    bool success = await likeIdea(idea.mId);
+                                    if (success) {
+                                      setState(() {
+                                        _future_list_ideas = fetchIdeas();
+                                      });
+                                    }
+                                  } catch (e) {
+                                    print("Error liking idea: $e");
+                                  }
+                                },
+                              ),
+                            ],
+                          ),
                         ),
-                      ),
-                    ),
-                  ],
+                        Divider(height: 1.0),
+                      ],
+                    );
+                  },
                 );
-              });
-        } else if (snapshot.hasError) {
-          // newly added
-          child = Text('${snapshot.error}');
-        } else {
-          // awaiting snapshot data, return simple text widget
-          // child = Text('Calculating answer...');
-          child = const CircularProgressIndicator(); //show a loading spinner.
-        }
-        return child;
-      },
+              } else {
+                return const Text('No data available');
+              }
+            },
+          ),
+        ),
+      ],
     );
-    return fb;
   }
 }
-//   @override
-//   Widget build_v1(BuildContext context){
-//     return ListView.builder(                        // ← 0
-//       padding: const EdgeInsets.all(16.0),
-//       itemCount: _words.length,                     // ← 1
-//       itemBuilder: /*1*/ (context, i) {             // ← 2
-//         developer.log('Building with context and $i', name: 'my.app.category');
 
-//         return Column(                              // ← 3
-//           children: <Widget>[
-//             ListTile(                               // ← 4
-//               title: Text(                          // ← 5
-//                 _words[i],                          // ← 6
-//                 style: _biggerFont,
-//               ),
-//             ),
-//             Divider(height: 1.0),                   // ← 7
-//           ], // end children
-//         ); // end Column
-//       }, // end itemBuilder
-//     ); // end ListView
-//   }
-//   @override
-//   Widget build_v0(BuildContext context){
-//     // for now, simply return a Text widget holding a random word pair
-//     final wordPair = WordPair.random();
-//     return Text(wordPair.asPascalCase);
-//   }
-// }
-//   @override
-//   Widget build_v2(BuildContext context) {
-//     var fb = FutureBuilder<List<String>>(
-//       future: _future_words,
-//       builder: (BuildContext context, AsyncSnapshot<List<String>> snapshot) {
-//         Widget child;
-//         if (snapshot.hasData) {
-//           developer.log('`using` ${snapshot.data}', name: 'my.app.category');
-//           // create  listview to show one row per array element of json response
-//           child = ListView.builder(
-//               //shrinkWrap: true, //expensive! consider refactoring. https://api.flutter.dev/flutter/widgets/ScrollView/shrinkWrap.html
-//               padding: const EdgeInsets.all(16.0),
-//               itemCount: snapshot.data!.length,
-//               itemBuilder: /*1*/ (context, i) {
-//                 return Column(
-//                   children: <Widget>[
-//                     ListTile(
-//                       title: Text(
-//                         snapshot.data![i],
-//                         style: _biggerFont,
-//                       ),
-//                     ),
-//                     Divider(height: 1.0),
-//                   ],
-//                 );
-//               });
-//         } else {
-//           // awaiting snapshot data, return simple text widget
-//           // child = Text('Calculating answer...');
-//           child = const CircularProgressIndicator(); // could show a loading spinner.
-//         }
-//         return child;
-//       },
-//     );
-//     return fb;
-//   }
-//   /* you may of course be keeping your other attempts build_v0 and build_v1 hanging around for reference for now */
-// }
+
 // method for trying out a long-running calculation
-Future<List<NumberWordPair>> doSomeLongRunningCalculation() async {
-  // return simpleLongRunningCalculation();  // we tried this, it worked
-  return getWebData();
-}
-
-Future<List<String>> simpleLongRunningCalculation() async {
-  await Future.delayed(Duration(seconds: 5)); // wait 5 sec
-  List<String> myList = List.generate(
-      100, (index) => WordPair.random().asPascalCase,
-      growable: true);
-  return myList;
-  // return ['x', 'y', 'z']; // you can hard code the result if you want it more predictable
-}
-// Future<List<NumberWordPair>> fetchNumberWordPairs() async {
-//   final response = await http
-//       .get(Uri.parse('https://team-untitled-23.dokku.cse.lehigh.edu/ideas'));
-//   if (response.statusCode == 200) {
-//     // If the server did return a 200 OK response, then parse the JSON.
-//     final List<NumberWordPair> returnData;
-//     var res = jsonDecode(response.body);
-//     print('json decode: $res');
-
-//     if( res is List ){
-//       print('test');
-//       returnData = (res as List<dynamic>).map( (x) => NumberWordPair.fromJson(x) ).toList();
-
-//     }else if( res is Map ){
-
-//       print('test2');
-//       returnData = <NumberWordPair>[NumberWordPair.fromJson(res as Map<String,dynamic>)];
-
-//     }else{
-//       print('test3');
-//       developer.log('ERROR: Unexpected json response type (was not a List or Map).');
-//       returnData = List.empty();
-
-//     }
-//     return returnData;
-//   } else {
-//     // If the server did not return a 200 OK response,
-//     // then throw an exception.
-//     throw Exception('Did not receive success status code from request.');
-//   }
-// }
