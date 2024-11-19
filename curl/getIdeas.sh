9bdcd12fe62415d5cd52969e276c247de8382a2e
Lily Fandre <lmf226@lehigh.edu>
2024-11-18 20:35:22 -0500

    working backend

--- curl/getIdeas.sh
+++
@@ -1,9 +0,0 @@
-## check if token is set
-if [ -z "$TOKEN" ]; then
-echo "Please set your token"
-exit 1
-fi
-
-curl -X GET https://team-untitled-23.dokku.cse.lehigh.edu/ideas \
--H "Content-Type: application/json" \
--H "Authorization: Bearer $TOKEN"
